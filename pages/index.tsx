8ae6d87fdb46a08ad082d8ec8b8197f98d3da226
socreative <socreative@rocketmail.com>
2023-09-03 03:41:23 +0100

    footer fixxxxx

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,7 @@ const Demo6: NextPage = () => {
                 <div className="mt-lg-n20 mt-xl-n22 mb-14 mb-md-16 position-relative">
                   <a
                     data-glightbox
-                    href="/"
+                    href="#"
                     className="btn btn-circle btn-primary btn-play ripple mx-auto mb-5 position-absolute"
                     style={{ top: '50%', left: '50%', transform: 'translate(-50%,-50%)', zIndex: 3 }}
                   >
