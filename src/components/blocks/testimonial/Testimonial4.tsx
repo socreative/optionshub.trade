7fd702a1b20f6b07ccb37fbbcf86b71722e5ec5f
socreative <socreative@rocketmail.com>
2023-09-03 02:55:44 +0100

    content tweaks

--- src/components/blocks/testimonial/Testimonial4.tsx
+++ src/components/blocks/testimonial/Testimonial4.tsx
@@ -12,9 +12,9 @@ const Testimonial4: FC = () => {
       <div className="container py-14 py-md-16">
         <div className="row">
           <div className="col-md-10 offset-md-1 col-lg-8 offset-lg-2 mx-auto text-center">
-            <h2 className="fs-15 text-uppercase text-muted mb-3">Happy Customers</h2>
+            <h2 className="fs-15 text-uppercase text-muted mb-3">Happy Together</h2>
             <h3 className="display-4 mb-6 px-xl-10 px-xxl-15">
-              Dont take our word for it. See what customers are saying about us.
+              Don't take our word for it. See what out team members are saying about us.
             </h3>
           </div>
         </div>
